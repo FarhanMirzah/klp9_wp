cf021f053b078d7fea267cd721b487031d0f0d61
FarhanMirzah <62887690+FarhanMirzah@users.noreply.github.com>
2022-05-08 19:46:11 +0700

    Add files via upload

---
+++ alternatif.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" contents="Kelompok 9 - SPK C">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <nav id="navigation">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="kriteria.php">Kriteria</a></li>
+                <li><a href="alternatif.php">Alternatif</a></li>
+                <li><a href="penilaian.php">Penilaian</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+    <div class="page">
+        <h2>Alternatif</h2>
+        <p>Ini adalah halaman Alternatif</p>
+    </div>
+</html>
---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" contents="Kelompok 9 - SPK C">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <nav id="navigation">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="kriteria.php">Kriteria</a></li>
+                <li><a href="alternatif.php">Alternatif</a></li>
+                <li><a href="penilaian.php">Penilaian</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+    <div class="page">
+        <h2>Selamat Datang</h2>
+        <p>Selamat datang di aplikasi SPK menggunakan metode WP.</p>
+        <p>Silahkan pilih menu disamping untuk melakukan pengolahan data.</p>
+    </div>
+</html>
